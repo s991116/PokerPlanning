063ef2883ab8a264eeff0570f772bbfaa109005e
Philip Juhl <philipjuhl@Philips-MBP.lan>
2020-03-28 21:59:28 +0100

    Moved all Requests and Socket to pokerRoutes.ts

--- server/src/poker-server.ts
+++ server/src/poker-server.ts
@@ -1,9 +1,6 @@
 import express from "express";
 import cors from "cors";
 import * as http from "http";
-import { Session, User, VotingState } from "./model";
-import path from "path";
-import { v4 as uuidv4 } from "uuid";
 import { Routes } from "./routes/pokerRoutes";
 
 const morgan = require("morgan");
@@ -11,194 +8,27 @@ const morgan = require("morgan");
 var bodyParser = require("body-parser");
 
 export class PokerServer {
-  public static readonly PORT:number = 8080;
   public routePrv: Routes = new Routes();
-
+  public server: http.Server;
   private app: express.Application;
-  private server: http.Server;
-  private io: SocketIO.Server;
-  private port: string | number;
-  private sessions : { [key: string]: Session }
-  private socketIdWithSession : { [sessionId: string]: string}
-
+ 
   constructor() {
     this.createApp();
-    this.config();
     this.createServer();
-    this.sockets();
-    this.listen();
-    this.sessions = {};
-    this.socketIdWithSession = {};
     this.routePrv.routes(this.app); 
-  }
-
-  private createNewSession(name: string) : string {
-    let session = new Session(uuidv4(), name);
-    this.sessions[session.id]=session;
-    return session.id;
-  }
-
-  private createNewUser(sessionId: string, socketId: string) : User {
-    this.socketIdWithSession[socketId] = sessionId;
-
-    let session = this.sessions[sessionId];
-    let userName = "User"+session.users.length;
-    let user = new User(uuidv4(), userName, socketId);
-    session.users.push(user);
-    return user;
-  }
-
-  private removeDisconnectedUser(socket: SocketIO.Socket) : void {
-    let sessionId = this.socketIdWithSession[socket.id];
-    if(sessionId)
-    { 
-      let users = this.sessions[sessionId].users;
-      if(users) {
-        users.forEach( (item, index) => {
-          if(item.socketId === socket.id) users.splice(index,1);
-        });
-        socket.in(sessionId).emit("status",this.sessions[sessionId]);
-      }
-    }
-  }
-
-  private createApp(): void {
-    this.app = express();
-    this.app.use(morgan("common"));
-    this.app.use(cors());
-    this.app.use(bodyParser.json())
-    
-    this.app.post('/createSession', (req, res) => {
-      let sessionId = this.createNewSession(req.body.sessionName);
-      this.io.in(sessionId).emit("status", this.sessions[sessionId]);
-      res.json({ sessionId: sessionId });
-    });
-
-    this.app.post('/createUser', (req, res) => {
-      let sessionId = req.body.sessionId;
-      let socketId = req.body.socketId;
-
-      if(this.sessions[sessionId])
-      {
-        let user = this.createNewUser(sessionId, socketId);
-        this.io.in(sessionId).emit("status", this.sessions[sessionId]);
-        res.json(user);  
-      }
-      else
-      {
-          res.status(400).json({
-            status: 'error',
-            error: 'sessionId do not exists',
-          });
-      }
-    });
-
-    this.app.post('/startVoting', (req, res) => {
-      let sessionId = req.body.id;
-      if(this.sessions[sessionId]) {
-        let session = this.sessions[sessionId];
-        session.state = VotingState.Voting;
-        this.io.in(sessionId).emit("status",this.sessions[sessionId]);
-        res.json(session);
-      }
-      else
-      {
-          res.status(400).json({
-            status: 'error',
-            error: 'sessionId do not exists',
-          });
-      }
-    });
-
-    this.app.post('/stopVoting', (req, res) => {
-      let sessionId = req.body.id;
-      if(this.sessions[sessionId]) {
-        let session = this.sessions[sessionId];
-        session.state = VotingState.Result;
-        this.io.in(sessionId).emit("status",this.sessions[sessionId]);
-        res.json(session);
-      }
-      else
-      {
-          res.status(400).json({
-            status: 'error',
-            error: 'sessionId do not exists',
-          });
-      }
-    });
-
-    this.app.post('/resetVoting', (req, res) => {
-      let sessionId = req.body.id;
-      if(this.sessions[sessionId]) {
-        let session = this.sessions[sessionId];
-        session.state = VotingState.WaitingToVote;
-        this.io.in(sessionId).emit("status",this.sessions[sessionId]);
-        res.json(session);
-      }
-      else
-      {
-          res.status(400).json({
-            status: 'error',
-            error: 'sessionId do not exists',
-          });
-      }
-    });
-
-    this.app.post('/vote', (req, res) => {
-      let sessionId = req.body.sessionId;
-      let userId = req.body.userId;
-      let cardValue = req.body.cardValue;
-      let session = this.sessions[sessionId];
-      if(session) {
-        let user = session.users.find(i => i.id === userId);
-        if(user) {
-          user.card = cardValue;
-          user.played = true;
-          this.io.in(sessionId).emit("status",this.sessions[sessionId]);
-          res.json(session);  
-        }
-        else
-        {
-            res.status(400).json({
-              status: 'error',
-              error: 'sessionId do not exists',
-            });
-        }
-      }
-      else
-      {
-          res.status(400).json({
-            status: 'error',
-            error: 'sessionId do not exists',
-          });
-      }
-    });
+    this.routePrv.sockets(this.server);
   }
 
   private createServer(): void {
     this.server = http.createServer(this.app);
   }
 
-  private config(): void {
-    this.port = process.env.PORT || PokerServer.PORT;
-  }
 
-  private sockets(): void {
-    this.io = require("socket.io").listen(this.server, { origins: '*:*'});
-  }
-
-  private listen(): void {
-    this.server.listen(this.port, () => {
-    });
-
-    this.io.on('connection', socket => {
-      socket.on('sessionRoom', (sessionRoomID) => {
-          socket.join(sessionRoomID);
-      });
-      socket.on('disconnect', () => {
-        this.removeDisconnectedUser(socket);
-      });
-    });
+  private createApp(): void {
+    this.app = express();
+    this.app.use(morgan("common"));
+    this.app.use(cors());
+    this.app.use(bodyParser.json())
   }
 
   public getApp(): express.Application {
--- server/src/routes/pokerRoutes.ts
+++ server/src/routes/pokerRoutes.ts
@@ -1,9 +1,80 @@
-// /lib/routes/crmRoutes.ts
 import {Request, Response, IRoute} from "express";
 import express from "express";
+import * as http from "http";
 import path from "path";
+import { v4 as uuidv4 } from "uuid";
+import { Session, User, VotingState } from "./../model";
 
-export class Routes {       
+export class Routes {   
+    
+    private io: SocketIO.Server;
+    private sessions : { [key: string]: Session }
+    private socketIdWithSession : { [sessionId: string]: string}
+    private port: string | number;
+
+    public static readonly PORT:number = 8080;
+
+    private config(): void {
+      this.port = process.env.PORT || Routes.PORT;
+    }
+    
+    constructor() {
+        this.config();
+        this.sessions = {};
+        this.socketIdWithSession = {}; 
+    }
+
+    private createNewSession(name: string) : string {
+        let session = new Session(uuidv4(), name);
+        this.sessions[session.id]=session;
+        return session.id;
+    }
+
+    private createNewUser(sessionId: string, socketId: string) : User {
+      this.socketIdWithSession[socketId] = sessionId;
+
+      let session = this.sessions[sessionId];
+      let userName = "User"+session.users.length;
+      let user = new User(uuidv4(), userName, socketId);
+      session.users.push(user);
+      return user;
+    }
+
+    private removeDisconnectedUser(socket: SocketIO.Socket) : void {
+      let sessionId = this.socketIdWithSession[socket.id];
+      if(sessionId)
+      { 
+        let users = this.sessions[sessionId].users;
+        if(users) {
+          users.forEach( (item: any, index: any) => {
+            if(item.socketId === socket.id) users.splice(index,1);
+          });
+          socket.in(sessionId).emit("status",this.sessions[sessionId]);
+        }
+      }
+    }
+    
+    private listen(server: any): void {
+      server.listen(Routes.PORT, () => {
+          console.log("Express server listening on port " + Routes.PORT);
+      });
+    
+      this.io.on('connection', socket => {
+        socket.on('sessionRoom', (sessionRoomID) => {
+          socket.join(sessionRoomID);
+        });
+      
+        socket.on('disconnect', () => {
+          this.removeDisconnectedUser(socket);
+        });
+      });
+    }
+
+    public sockets(server:any): void {
+        this.io = require("socket.io").listen(server, { origins: '*:*'});
+        this.listen(server);
+    }
+    
     public routes(app:any): void {   
 
         var htmlPath = path.resolve(__dirname + "./../../../client/dist/client/");        
@@ -15,14 +86,111 @@ export class Routes {
         app.route('/session/*').get(function (req:any, res:any) {
             res.status(200).sendFile(`/`, {root: htmlPath});
         });
+        
+        app.route('/createSession').post((req:any, res:any) => {
+            let sessionId = this.createNewSession(req.body.sessionName);
+            this.io.in(sessionId).emit("status", this.sessions[sessionId]);
+            res.json({ sessionId: sessionId });
+        });
 
-/*
-        app.route('/')
-        .get((req: Request, res: Response) => {            
-            res.status(200).send({
-                message: 'GET request successfulll!!!!'
-            })
-        })              
-*/ 
+        app.route('/createUser').post((req: any, res: any) => {
+            let sessionId = req.body.sessionId;
+            let socketId = req.body.socketId;
+      
+            if(this.sessions[sessionId])
+            {
+              let user = this.createNewUser(sessionId, socketId);
+              this.io.in(sessionId).emit("status", this.sessions[sessionId]);
+              res.json(user);  
+            }
+            else
+            {
+                res.status(400).json({
+                  status: 'error',
+                  error: 'sessionId do not exists',
+                });
+            }
+        });
+      
+        app.route('/startVoting').post((req: any, res: any) => {
+            let sessionId = req.body.id;
+            if(this.sessions[sessionId]) {
+              let session = this.sessions[sessionId];
+              session.state = VotingState.Voting;
+              this.io.in(sessionId).emit("status",this.sessions[sessionId]);
+              res.json(session);
+            }
+            else
+            {
+                res.status(400).json({
+                  status: 'error',
+                  error: 'sessionId do not exists',
+                });
+            }
+          });
+      
+          app.route('/stopVoting').post((req: any, res: any) => {
+            let sessionId = req.body.id;
+            if(this.sessions[sessionId]) {
+              let session = this.sessions[sessionId];
+              session.state = VotingState.Result;
+              this.io.in(sessionId).emit("status",this.sessions[sessionId]);
+              res.json(session);
+            }
+            else
+            {
+                res.status(400).json({
+                  status: 'error',
+                  error: 'sessionId do not exists',
+                });
+            }
+          });
+      
+          app.route('/resetVoting').post((req: any, res: any) => {
+            let sessionId = req.body.id;
+            if(this.sessions[sessionId]) {
+              let session = this.sessions[sessionId];
+              session.state = VotingState.WaitingToVote;
+              this.io.in(sessionId).emit("status",this.sessions[sessionId]);
+              res.json(session);
+            }
+            else
+            {
+                res.status(400).json({
+                  status: 'error',
+                  error: 'sessionId do not exists',
+                });
+            }
+          });
+      
+          app.route('/vote').post((req: any, res: any) => {
+            let sessionId = req.body.sessionId;
+            let userId = req.body.userId;
+            let cardValue = req.body.cardValue;
+            let session = this.sessions[sessionId];
+            if(session) {
+              let user = session.users.find((i: any) => i.id === userId);
+              if(user) {
+                user.card = cardValue;
+                user.played = true;
+                this.io.in(sessionId).emit("status",this.sessions[sessionId]);
+                res.json(session);  
+              }
+              else
+              {
+                  res.status(400).json({
+                    status: 'error',
+                    error: 'sessionId do not exists',
+                  });
+              }
+            }
+            else
+            {
+                res.status(400).json({
+                  status: 'error',
+                  error: 'sessionId do not exists',
+                });
+            }
+          });
     }
 }
