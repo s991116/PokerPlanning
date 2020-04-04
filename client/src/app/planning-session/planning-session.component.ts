9641620db9980793bb32ecefda04a33025585ada
Philip Juhl <philipjuhl@Philips-MBP.lan>
2020-04-05 00:44:47 +0200

    Detect if Session exists and inform if it do not

--- client/src/app/planning-session/planning-session.component.ts
+++ client/src/app/planning-session/planning-session.component.ts
@@ -1,89 +1,93 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import * as io from 'socket.io-client';
-import { Inject, Injectable } from '@angular/core';
-import { SESSION_STORAGE, StorageService } from 'ngx-webstorage-service';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import * as io from "socket.io-client";
+import { Inject, Injectable } from "@angular/core";
+import { SESSION_STORAGE, StorageService } from "ngx-webstorage-service";
+import { HttpClient } from "@angular/common/http";
 import { Session, VotingState } from "./../model/";
 
-const USERNAME_SESSION_KEY = 'UserInfo';
+const USERNAME_SESSION_KEY = "UserInfo";
 
 @Component({
-  selector: 'app-planning-session',
-  templateUrl: './planning-session.component.html',
-  styleUrls: ['./planning-session.component.css']
+  selector: "app-planning-session",
+  templateUrl: "./planning-session.component.html",
+  styleUrls: ["./planning-session.component.css"]
 })
 export class PlanningSessionComponent implements OnInit {
-  sessionId:string;
-  sessionName:string;
-  userDefined:boolean;
-  userName:string;
-  userId:string;
-  session:Session;
+  sessionId: string;
+  sessionName: string;
+  userDefined: boolean;
+  sessionExists: boolean;
+  userName: string;
+  userId: string;
+  session: Session;
   startVotingDisabled: boolean;
   stopVotingDisabled: boolean;
   resetVotingDisabled: boolean;
 
-  constructor(private _Activatedroute:ActivatedRoute, @Inject(SESSION_STORAGE) private storage: StorageService, private http: HttpClient) { 
-      this._Activatedroute.paramMap.subscribe(params => { 
-      this.sessionId = params.get('id'); 
+  constructor(
+    private _Activatedroute: ActivatedRoute,
+    @Inject(SESSION_STORAGE) private storage: StorageService,
+    private http: HttpClient
+  ) {
+    this._Activatedroute.paramMap.subscribe(params => {
+      this.sessionId = params.get("id");
       console.log("Room Session Id:" + this.sessionId);
       this.session = new Session("", "");
     });
+    this.sessionExists = false;
   }
 
   startVotingForm(): void {
-    this.http.post("/startVoting",this.session)
-    .subscribe(
-        (val: any) => {            
-        },
-        response => {
-            console.log("POST call in error", response);
-        },
-        () => {
-        });
+    this.http.post("/startVoting", this.session).subscribe(
+      (val: any) => {},
+      response => {
+        console.log("POST call in error", response);
+      },
+      () => {}
+    );
   }
 
   stopVotingForm(): void {
-    this.http.post("/stopVoting",this.session)
-    .subscribe(
-        (val: any) => {            
-        },
-        response => {
-            console.log("POST call in error", response);
-        },
-        () => {
-        });
+    this.http.post("/stopVoting", this.session).subscribe(
+      (val: any) => {},
+      response => {
+        console.log("POST call in error", response);
+      },
+      () => {}
+    );
   }
 
   resetVotingForm(): void {
-    this.http.post("/resetVoting",this.session)
-    .subscribe(
-        (val: any) => {            
-        },
-        response => {
-            console.log("POST call in error", response);
-        },
-        () => {
-        });
+    this.http.post("/resetVoting", this.session).subscribe(
+      (val: any) => {},
+      response => {
+        console.log("POST call in error", response);
+      },
+      () => {}
+    );
   }
 
   voteForm(frmElement): void {
     let cardValue = frmElement.form.value.valueName;
     console.log(cardValue);
-    this.http.post("/vote",{sessionId: this.sessionId, userId: this.userId, cardValue: cardValue})
-    .subscribe(
-        (val: any) => {
-        },
+    this.http
+      .post("/vote", {
+        sessionId: this.sessionId,
+        userId: this.userId,
+        cardValue: cardValue
+      })
+      .subscribe(
+        (val: any) => {},
         response => {
-            console.log("POST call in error", response);
+          console.log("POST call in error", response);
         },
-        () => {
-        });
+        () => {}
+      );
   }
 
   setButtonState(state: VotingState): void {
-    switch(state) {
+    switch (state) {
       case VotingState.WaitingToVote:
         this.startVotingDisabled = false;
         this.stopVotingDisabled = true;
@@ -101,32 +105,38 @@ export class PlanningSessionComponent implements OnInit {
         this.stopVotingDisabled = true;
         this.resetVotingDisabled = false;
         break;
-      
     }
   }
 
   ngOnInit(): void {
     this.userDefined = this.storage.has(USERNAME_SESSION_KEY);
     const socket = io();
-    socket.on('connect', () => {
-      socket.emit('sessionRoom', this.sessionId);
+    socket.on("connect", () => {
+      socket.emit("sessionRoom", this.sessionId);
 
-      if(!this.userDefined) {
-        this.http.post("/createUser",{sessionId: this.sessionId, socketId: socket.id})
-        .subscribe(
+      if (!this.userDefined) {
+        this.http
+          .post("/createUser", {
+            sessionId: this.sessionId,
+            socketId: socket.id
+          })
+          .subscribe(
             (val: any) => {
-                this.userName = val.name; 
-                this.userId = val.id;        
+              this.userName = val.name;
+              this.userId = val.id;
+              this.sessionExists = true;
             },
             response => {
-                console.log("POST call in error", response);
+              this.sessionExists = false;
+              console.log("POST call in error", response);
             },
-            () => {
-            });
-          };
+            () => {}
+          );
+      }
     });
 
-    socket.on('status', (data) => {
+    socket.on("status", data => {
+      console.log(data);
       this.session = data as Session;
       this.sessionName = this.session.name;
       this.setButtonState(this.session.state);
