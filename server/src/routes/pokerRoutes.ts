00a9465f632e5fc62583bf1034ba96c81373e612
Philip Juhl <philipjuhl@Philips-MBP.lan>
2020-05-08 13:53:37 +0200

    cosmetic

--- server/src/routes/pokerRoutes.ts
+++ server/src/routes/pokerRoutes.ts
@@ -1,4 +1,4 @@
-import { Request, Response, IRoute } from "express";
+import { Request, Response } from "express";
 import express from "express";
 import * as http from "http";
 import path from "path";
